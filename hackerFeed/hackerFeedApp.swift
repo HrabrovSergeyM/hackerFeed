24fac3ad84b9041619cadea0dd264de151ab7bdf
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-03-12 14:30:31 +0400

    Initial Commit

---
+++ hackerFeed/hackerFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerFeedApp.swift
+//  hackerFeed
+//
+//  Created by Sergey Hrabrov on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct hackerFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
